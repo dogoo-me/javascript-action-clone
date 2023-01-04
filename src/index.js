647a51a8d13167a602ee4227ad79d009c2b7993c
esllo <master@esllo.com>
2023-01-04 14:03:31 +0900

    feat: print github context

--- src/index.js
+++ src/index.js
@@ -1,68 +1,18 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec')
-const glob = require('@actions/glob')
+const github = require('@actions/github')
 
 
-// most @actions toolkit packages have async methods
 async function run() {
   try { 
-    // input/outputs
     core.startGroup('input/outputs')
 
-    const fieldA = core.getInput('fieldA');
-    const fieldB = core.getInput('fieldB');
-    core.info(`fieldA(${typeof fieldA}): ${fieldA}`);
-    core.info(`fieldB(${typeof fieldB}): ${fieldB}`);
+    // const token = core.getInput('github_token')
 
-    core.endGroup()
-
-    // logging
-    core.startGroup('logging')
-
-    console.log('this is console log')
-    core.info('this is info text')
-    core.debug('this is debug text')
-    core.warning('this is warning text')
-    core.error('this is error text')
-    core.notice('this is notice text')
-
-    core.endGroup()
-
-    core.startGroup('logging/ansi color')
-
-    core.info('\033[38;2;255;0;0mred\033[0m')
-    core.info('\033[38;2;0;255;0mgreen\033[0m')
-    core.info('\033[38;2;0;0;255mblue\033[0m')
-
-    core.endGroup()
-
-    // exec
-    core.startGroup('@actions/exec')
-    const pwdExitCode = await exec.exec('pwd')
-    core.info(`pwd exit with ${pwdExitCode}`)
+    // const octokit = github.getOctokit(token)
     
-    let lsOutput = ''
-    const options = {
-      listeners: {
-        stdout: (data) => {
-          lsOutput += data.toString()
-        }
-      }
-    }
+    const fields = ['action', 'actor', 'eventName', 'issue', 'job', 'payload', 'ref', 'repo', 'runId', 'runNumber', 'workflow']
 
-    await exec.exec('ls', ['-l'], options)
-    core.info(`lsOutput:\n${lsOutput}`)
-
-    core.endGroup()
-
-    core.startGroup('@actions/glob')
-
-    const patterns = ['**.txt', '**/*test*']
-    const globber = await glob.create(patterns.join('\n'))
-
-    for await (const file of globber.globGenerator()) {
-      core.info(file)
-    }
+    fields.forEach((field) => core.info(`${field}: ${github.context[field]}`))
 
     core.endGroup()
   } catch (error) {
